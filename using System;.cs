05c0b9ae0156fca098887cf1c1547ccda8a8d542
Koronenko Anastasiia <koronenko.nastya@gmail.com>
2020-08-04 10:30:25 +0300

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Diagnostics;
+
+namespace ProjectOne 
+{
+class MClass 
+    {
+            public static void Main(string[] args) 
+        {
+            string a = "test";
+            System.Console.WriteLine(a);
+           
+        } 
+    }
+
+}
